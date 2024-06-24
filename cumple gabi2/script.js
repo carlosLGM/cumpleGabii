54ee802be6e3a62d01da299b3d1f80fba9bbf840
carlosLGM <100099447+carlosLGM@users.noreply.github.com>
2024-06-23 23:05:15 -0600

    a

---
+++ cumple gabi2/script.js
@@ -0,0 +1,7 @@
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    alert('¡Feliz Cumpleaños, Gabi! 🎉');
+});
+
+document.getElementById('collageButton').addEventListener('click', function() {
+    window.location.href = 'fotos/collage.html';
+});
